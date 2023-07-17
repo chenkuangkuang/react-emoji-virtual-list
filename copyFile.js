9a83e606055c2e31982c957b7ca182457c0c1719
chenkuangkuang <540342678@qq.com>
2023-07-17 11:46:54 +0800

    update version

--- copyFile.js
+++ copyFile.js
@@ -2,9 +2,9 @@ var fs = require("fs");
 
 fs.mkdir("./dist/svg", function (err) {
   if (err) return console.log("mkdir err", err);
-  fs.copyFile('./src/svg/CategoryNav.svg', './dist/static/CategoryNav.svg', function (err) {
+  fs.copyFile('./src/svg/CategoryNav.svg', './dist/svg/CategoryNav.svg', function (err) {
     if (err) console.log('something wrong was happened:', err)
-    else console.log('copy file succeed');
+    else console.log('copy file successed');
   });
 })
 
